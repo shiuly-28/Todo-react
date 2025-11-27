a10f60c385e28a7b079a890c6596c5c7ca7206f8
shiuly-28 <shulybd1245@gmail.com>
2025-11-27 22:44:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from "./Component/Header.jsx";
+import HeroSection from "./Component/HeroSection.jsx";
+import Footer from "./Component/Footer.jsx";
+
+
+export default function App() {
+    return (
+        <div className='min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50'>
+            <Header />
+            <div className="flex flex-col justify-center items-center px-4 py-8">
+                <HeroSection />
+               <taskBoard/>
+            </div>
+            <Footer/>
+        </div>
+    )
+}
